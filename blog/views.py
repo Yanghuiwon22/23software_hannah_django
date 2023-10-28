5dd142d3538170d280bb8ddb18006a741fe8b9cd
Yanghuiwon22 <hannah4212@naver.com>
2023-10-28 19:36:59 +0900

    #과제12 완료

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DeleteView
 from .models import Post
 
-# Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+class PostDetail(DeleteView):
+    model = Post
+# Create your views here.
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
