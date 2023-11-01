2cddab4f6f8e6f4200dfc3bb5de12b6259be2960
Yanghuiwon22 <hannah4212@naver.com>
2023-11-01 09:23:39 +0900

    #과제13 2차수정 완.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
