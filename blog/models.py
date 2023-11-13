511090a26d929514c020a06678b1e7dd800ad863
Yanghuiwon22 <hannah4212@naver.com>
2023-11-13 17:18:52 +0900

    포스트 상세 페이지에 대표 이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    # TextField의 Text는 많을 글을 의미한다. (제한이 없다.)
-    # CharField의 Char는 글자수에 제한이 있다.
-
-    # 왜 구분을 했는냐?
-    # ---> 필요한 정보를 찾을 때 편리하다.
-    # ---> 용량면으로 이점이 있다.
-
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
